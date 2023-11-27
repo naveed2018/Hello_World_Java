468a3fa249672fa2b97d73c97bf4e956b0aaa2c4
naveed2018 <45180899+naveed2018@users.noreply.github.com>
2023-11-27 10:58:04 +0000

    Update HelloWorld.java
    Adding a single statment for testing jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld
   public static void main(String[] args) 
   	{
     	System.out.println("Hello, From the World of DevOps");
+      System.out.println("This is a new line addition to the program");
+      
   	}
 
 }
